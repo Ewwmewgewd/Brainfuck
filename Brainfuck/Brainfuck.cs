50d23a3e41aa8efcf046f8c4e3f8300b2124b3c8
Lazar Petrović <lazar.petrovic2310@gmail.com>
2022-01-03 22:30:39 +0100

    Add project files.

---
+++ Brainfuck/Brainfuck.cs
@@ -0,0 +1,96 @@
+﻿namespace Brainfuck;
+
+public class Brainfuck
+{
+    private int _pointer;
+    private int _inputPointer;
+    private string _code;
+    private string _input;
+    private string _output;
+    private readonly int _tapeSize;
+    private byte[] _tape;
+
+    public void SetCode(string code) { _code = code; }
+    public void SetInput(string input) { _input = input; }
+    public string GetOutput() { return _output; }
+
+    public Brainfuck(string code, string input = "", int tapeSize = 2000, bool autorun = true)
+    {
+        _pointer = 0;
+        _inputPointer = 0;
+        _code = code;
+        _input = input;
+        _output = string.Empty;
+        _tapeSize = tapeSize;
+        _tape = new byte[_tapeSize];
+
+        if (autorun)
+            Run();
+    }
+
+    public void Run()
+    {
+        int codeLen = _code.Length;
+
+        if (codeLen == 0)
+            return;
+
+        for (int i = 0; i < codeLen; i++)
+        {
+            if ((_code[i] == '[') && (_tape[_pointer] == 0))
+            {
+                int skip = 0;
+                int chr = i + 1;
+                while ((skip > 0) || (_code[chr] != ']'))
+                {
+                    if (_code[chr] == '[')
+                        skip++;
+                    else if (_code[chr] == ']')
+                        skip--;
+                    chr++;
+                }
+
+                i = chr;
+                continue;
+            }
+            else if ((_code[i] == ']') && (_tape[_pointer] != 0))
+            {
+                int skip = 0;
+                int chr = i - 1;
+                while ((skip > 0) || (_code[chr] != '['))
+                {
+                    if (_code[chr] == ']')
+                        skip++;
+                    else if (_code[chr] == '[')
+                        skip--;
+                    chr--;
+                }
+
+                i = chr;
+                continue;
+            }
+
+            switch (_code[i])
+            {
+                case '>': if (_pointer >= _tapeSize - 1) _pointer = 0; else _pointer++; break;
+                case '<': if (_pointer <= 0) _pointer = _tapeSize - 1; else _pointer--; break;
+                case '+': _tape[_pointer]++; break;
+                case '-': _tape[_pointer]--; break;
+                case ',': if (_inputPointer < _input.Length) _tape[_pointer] = (byte)_input[_inputPointer++]; break;
+                case '.': _output += (char)_tape[_pointer]; break;
+                default: break;
+            }
+        }
+
+        return;
+    }
+
+    public void Reset()
+    {
+        _pointer = 0;
+        _code = string.Empty;
+        _input = string.Empty;
+        _output = string.Empty;
+        _tape = new byte[_tapeSize];
+    }
+}
---
+++ Brainfuck/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Brainfuck;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if ((args.Length < 1) || (args.Length > 2))
+        {
+            Console.WriteLine(
+                "Usage:   Brainfuck.exe FilePath(code)    [FilePath(input)]\n" +
+                "Example: Brainfuck.exe source\\code.bf    inputs.txt");
+            return;
+        }
+
+        // Get code string.
+        if (!File.Exists(args[0]))
+        {
+            Console.WriteLine($"Error: File '{args[0]}' couldn't be found.");
+            return;
+        }
+        
+        string code = File.ReadAllText(args[0]);
+
+        // Get input string.
+        string input = string.Empty;
+        if (args.Length == 2)
+        {
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine($"Error: File '{args[1]}' couldn't be found.");
+                return;
+            }
+
+            input = File.ReadAllText(args[1]);
+        }
+
+        // Interpret code.
+        var bf = new Brainfuck(code, input);
+        Console.WriteLine(bf.GetOutput());
+
+        // In case you are dragging & dropping files onto the .exe
+        Console.WriteLine("\nPress any key to exit.");
+        Console.ReadKey();
+    }
+}
